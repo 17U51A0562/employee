89b945ea7a9f44edcdf7a619eae34d149501dcc8
17U51A0562 <vikasmudhiraj283@gmail.com>
2021-10-01 20:48:26 +0530

    done employee wage and line program

---
+++ linecomparison/WelcometoLinecomaprison.java
@@ -0,0 +1,11 @@
+/*
+ * Welcome to Line Comparison Computation Program
+ */
+package linecomparison;
+
+public class WelcometoLinecomaprison {
+    public static void main(String[] args) {  
+	System.out.println("Welcome to Line Comparison Computation Program");
+    }
+	}
+
